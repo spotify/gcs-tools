343bd9a71245b46222cd7100cfd8f22c4abb60e4
Neville Li <neville.lyh@gmail.com>
2021-03-15 17:00:08 -0400

    add magnolify-tools to make-binary.sh

--- make-binary.sh
+++ make-binary.sh
@@ -4,10 +4,11 @@ sbt clean assembly
 
 rm -rf bin
 mkdir bin
-cp avro-tools/target/scala-*/avro-tools-*.jar parquet-cli/target/scala-*/parquet-cli-*.jar proto-tools/target/scala-*/proto-tools-*.jar bin
+cp avro-tools/target/scala-*/avro-tools-*.jar parquet-cli/target/scala-*/parquet-cli-*.jar proto-tools/target/scala-*/proto-tools-*.jar magnolify-tools/target/scala-*/magnolify-tools-*.jar bin
 cp scripts/gcs-tools bin
 
 cd bin
 ln -s gcs-tools avro-tools
 ln -s gcs-tools parquet-cli
 ln -s gcs-tools proto-tools
+ln -s gcs-tools magnolify-tools
